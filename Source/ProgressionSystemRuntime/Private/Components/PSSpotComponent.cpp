911f689c0d341e28064f0e7da53db4cb67e6afc7
valeriy <rotermel.valera@gmail.com>
2025-02-26 02:02:32 +0200

    used int32 instead of int

--- Source/ProgressionSystemRuntime/Private/Components/PSSpotComponent.cpp
+++ Source/ProgressionSystemRuntime/Private/Components/PSSpotComponent.cpp
@@ -147,7 +147,7 @@ void UPSSpotComponent::RefreshAmountOfUnlockedSkins(bool bApplySkin)
 	UMySkeletalMeshComponent& SpotMeshComponent = GetMeshChecked();
 	const int32 UnlockedSkinsAmount = UPSWorldSubsystem::Get().GetCurrentSaveToDiskRowByName().UnlockedSkinsAmount;
 
-	for (int Index = 0; Index < UnlockedSkinsAmount; Index++)
+	for (int32 Index = 0; Index < UnlockedSkinsAmount; Index++)
 	{
 		SpotMeshComponent.SetSkinAvailable(true, Index);
 		if (bApplySkin)
